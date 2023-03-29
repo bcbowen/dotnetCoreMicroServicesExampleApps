ec0362c3973055efe69ba2ca2c1a5ec04cfb51fb
Ben Bowen <bcbowen@gmail.com>
2023-03-28 21:51:47 -0400

    Start implementing product api

--- MangoRestaurant/Mango.Services.ProductAPI/Program.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Program.cs
@@ -1,9 +1,18 @@
+using AutoMapper;
+using Mango.Services.ProductAPI;
 using Mango.Services.ProductAPI.DBContexts;
+using Mango.Services.ProductAPI.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper); 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>(); 
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- MangoRestaurant/Mango.Services.ProductAPI/Repositories/IProductRepository.cs
+++ MangoRestaurant/Mango.Services.ProductAPI/Repositories/IProductRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace Mango.Services.ProductAPI.Repositories
+﻿using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI.Repositories
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int id);
+        Task<ProductDto> CreateUpdateProduct(ProductDto product); 
+        Task<bool> DeleteProduct(int productId);
     }
 }
