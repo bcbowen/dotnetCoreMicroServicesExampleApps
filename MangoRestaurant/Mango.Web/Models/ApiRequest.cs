a086c4e0a192bdda696098527aaf325ed9ccd536
Ben Bowen <bcbowen@gmail.com>
2023-04-08 19:39:22 -0400

    Working on product api

---
+++ MangoRestaurant/Mango.Web/Models/ApiRequest.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Web;
+using static Mango.Web.SD;
+
+
+
+public class ApiRequest
+{
+    public ApiType ApiType { get; set; } = ApiType.GET;
+
+    public string MyProperty { get; set; }
+}
+
---
+++ MangoRestaurant/Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Mango.w.ProductAPI.Models.Dto;
+using Mango.Web.Models.Dto;
+
+namespace Mango.Web.Services.NewFolder
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+
+        Task<T> CreateProductAsync<T>(ProductDto product);
+        Task<T> UpdateProductAsync<T>(ProductDto product);
+        Task<T> DeleteProductAsync<T>(int id);
+
+    }
+}
